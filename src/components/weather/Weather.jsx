d1dbf12482f8f3bfc8d173378338d7b8b4fa9980
arina <ariadna20080609@gmail.com>
2025-08-28 15:16:05 +0300

    add card

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -7,26 +7,27 @@ import { IoMdRefresh } from "react-icons/io";
 
 const Weather = () => {
   return (
-     <main className="main" >
-   <div className="main-card"></div>
-   <div className="main-card-title">
-    <h2 className="main-card-title-country">Ukraine</h2>
-      <h2 className="main-card-title-city">Kyiv</h2>
-   </div>
-   <h2 className="main-card-time">18:00</h2>
- <div className="main-card-forecast">
-      <h3 className="main-card-weather">Hourly forecast</h3>
-    <h3 className="main-card-weather">Weekly forecast</h3>
- </div>
-   <p className="main-card-date"> 22.08.2025 | Friday</p>
-   <img src={FiSun} alt="" className="main-card-icon" />
-   <h2 className="main-card-temperature">22℃ </h2>
-   <div className="main-card-function">
-    <img src={IoMdRefresh} alt="" className="main-card-function-reload" />
-    <img src={FaRegHeart} alt="" className="main-card-function-like" />
-    <button className="main-card-function-more">See more</button>
-    <img src={FaRegTrashCan} alt="" className="main-card-function-delete" />
-   </div>
+    <main className="main" >
+      <div className="main-card">
+      <div className="main-card-title">
+        <h2 className="main-card-title-country">Ukraine</h2>
+        <h2 className="main-card-title-city">Kyiv</h2>
+      </div>
+      <h2 className="main-card-time">18:00</h2>
+      <div className="main-card-forecast">
+        <h3 className="main-card-weather">Hourly forecast</h3>
+        <h3 className="main-card-weather">Weekly forecast</h3>
+      </div>
+      <p className="main-card-date"> 22.08.2025 | Friday</p>
+      <FiSun className="main-card-icon" />
+      <h2 className="main-card-temperature">22℃ </h2>
+      <div className="main-card-function">
+        <IoMdRefresh className="main-card-function-reload" />
+        <FaRegHeart className="main-card-function-like" />
+        <button className="main-card-function-more">See more</button>
+        <FaRegTrashCan className="main-card-function-delete" />
+      </div>
+       </div>
     </main>
   );
 };
